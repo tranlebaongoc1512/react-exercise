7d8cce96268a7ab1cf0afe3992b76d3553cc26b5
tranlebaongoc1512 <tranlebaongoc1512@gmail.com>
2023-05-16 13:28:05 +0700

    Functional Components

--- src/components/Player.js
+++ src/components/Player.js
@@ -8,7 +8,7 @@ export default function Player() {
                 return (
                     <div className='column'>
                         <div className='card'>
-                            <img src={player.img} />
+                            <img src={player.img} alt={player.name}/>
                             <h3>{player.name}</h3>
                             <p className='title'>{player.club}</p>
                             <p><button>Detail</button></p>
